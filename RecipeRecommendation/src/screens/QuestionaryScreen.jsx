9f9a52af65d68268f2648b92c99da5a0aeac29ac
luciaPG <93549329+luciaPG@users.noreply.github.com>
2024-05-28 15:50:05 +0200

    Main Part Questionary Done

--- RecipeRecommendation/src/screens/QuestionaryScreen.jsx
+++ RecipeRecommendation/src/screens/QuestionaryScreen.jsx
@@ -3,19 +3,38 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable linebreak-style */
 import './QuestionaryScreen.css';
-import { Button } from '@chakra-ui/react'
-import {
-    FormControl,
-    FormLabel,
-    Radio,
-    HStack,
-    Stack,
-    RadioGroup,
-    FormHelperText
-} from '@chakra-ui/react'
+import questionary from '/src/assets/questionary.json'
+import React, { useState } from 'react';
+import { Card, CardBody, Text, FormControl, FormLabel, RadioGroup, Stack, Radio, Button } from '@chakra-ui/react';
 import { PiPlantFill } from "react-icons/pi";
+import cardTheme from './CardStyling.jsx'
 
 const QuestionaryScreen = () => {
+    const [answers, setAnswers] = useState({});
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    
+
+    const handleOptionChange = (option, questionIndex) => {
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            [questionIndex]: option
+        }));
+    };
+
+    const handlePreviousQuestion = () => {
+        setCurrentQuestion(prevQuestion => Math.max(prevQuestion - 1, 0));
+    };
+
+    const handleNextQuestion = () => {
+        setCurrentQuestion(prevQuestion => Math.min(prevQuestion + 1, questionary.questions.length - 1));
+    };
+
+    function finishFunction(){
+        console.log(answers)
+        if( Object.keys(answers).length!==15){
+            console.log('alert')
+        }
+    }
     return (
         <div className="Home">
             <div className='topBar'>
@@ -27,176 +46,40 @@ const QuestionaryScreen = () => {
                     </div>
                 </div>
             </div>
-            <div className='form'>
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md'>
-                    <FormLabel as='legend' color='black'>1. How would you describe your body frame?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Thin and lanky</Radio>
-                            <Radio className='radio' value='b'>b)Medium build</Radio>
-                            <Radio className='radio' value='c'>c)Solid and sturdy</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
+            <Card {...cardTheme.card}>
+                <CardBody {...cardTheme.cardBody}>
+                    <Text className='question'>{questionary.questions[currentQuestion].question}</Text>
+                    {questionary.questions[currentQuestion].options.map((option, index) => (
+                        <Button
+                            key={index}
+                            padding={'1px'}
+                            border={answers[currentQuestion] === option ? '0px' : 'none'}
+                            bg={answers[currentQuestion] === option ? 'lightgray' : 'white'}
+                            _hover={{ bg: answers[currentQuestion] === option ? 'lightgray' : 'white', border: 'none' }}
+                            _focus={{ outline: 'none' }}
+                            onClick={() => handleOptionChange(option, currentQuestion)}
+                        >
+                            <Text style={{ color: 'black' }}>{option}</Text>
+                        </Button>
+                    ))}
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: '2rem', marginLeft: '2rem' }}>
+                        <Button {...cardTheme.btn} colorScheme='teal' size='md' mt={4} onClick={() => handlePreviousQuestion()}>
+                            Return
+                        </Button>
+                        {currentQuestion < questionary.questions.length - 1 ? (
+                            <Button {...cardTheme.btn} colorScheme='teal' size='md' mt={4} onClick={handleNextQuestion}>
+                                Continue
+                            </Button>
+                        ) : (
+                            <Button {...cardTheme.btn} colorScheme='teal' size='md' mt={4} onClick={()=>finishFunction()}>
+                                Finish
+                            </Button>
+                        )}
+                    </div>
 
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>2. What is your typical appetite like?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                         <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Variable, sometimes strong, sometimes weak</Radio>
-                            <Radio className='radio' value='b'>b)Strong and consistent</Radio>
-                            <Radio className='radio' value='c'>c)Slow and steady</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
+                </CardBody>
+            </Card>
 
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>3. How is your digestion?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Irregular, prone to gas and bloating</Radio>
-                            <Radio className='radio' value='b'>b)Strong, can eat almost anything</Radio>
-                            <Radio className='radio' value='c'>c)Slow, tendency towards heaviness after meals</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>4. How do you generally handle stress?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Become anxious or worried easily</Radio>
-                            <Radio className='radio' value='b'>b)Tend to get angry or irritable</Radio>
-                            <Radio className='radio' value='c'>c)Prefer to avoid conflict, may become withdrawn</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>5. How do you sleep?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Light sleeper, prone to insomnia</Radio>
-                            <Radio className='radio' value='b'>b)Deep sleeper, but may wake up easily</Radio>
-                            <Radio className='radio' value='c'>c)Heavy sleeper, difficulty waking up in the morning</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>6. How is your skin?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>
-                            <Radio className='radio' value='a'>a)Dry, prone to cracking</Radio>
-                            <Radio className='radio' value='b'>b)Warm, sensitive, prone to rashes</Radio>
-                            <Radio className='radio' value='c'>c)Oily, prone to acne or congestion</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>7. How is your hair?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Dry, thin, and prone to split ends</Radio>
-                            <Radio className='radio' value='b'>b)ine, but prone to premature graying or balding</Radio>
-                            <Radio className='radio' value='c'>c)Thick, oily, and lustrous</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>8. How do you handle changes in weather?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Tend to feel cold easily, especially in windy weather</Radio>
-                            <Radio className='radio' value='b'>b)Tend to feel hot easily, especially in humid weather</Radio>
-                            <Radio className='radio' value='c'>c)Tend to feel sluggish in damp or cold weather</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>9. What is your typical energy level throughout the day?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)High energy in short bursts, followed by fatigue</Radio>
-                            <Radio className='radio' value='b'>b)Moderate and steady energy throughout the day</Radio>
-                            <Radio className='radio' value='c'>c)Low energy, especially in the mornings</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>10. How do you prefer to spend your free time?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Engaging in stimulating activities or hobbies</Radio>
-                            <Radio className='radio' value='b'>b)Exercising or being physically active</Radio>
-                            <Radio className='radio' value='c'>c)Relaxing or practicing mindfulness</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>11. How is your memory?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Quick to learn, but also quick to forget</Radio>
-                            <Radio className='radio' value='b'>b)Sharp and precise</Radio>
-                            <Radio className='radio' value='c'>c)Slow to learn, but once learned, tends to retain information well</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>12. How do you respond to new experiences or changes?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Tend to feel anxious or overwhelmed</Radio>
-                            <Radio className='radio' value='b'>b)Tend to feel competitive or ambitious</Radio>
-                            <Radio className='radio' value='c'>c)Tend to feel cautious or resistant</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>13. What is your natural body temperature like?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Cold hands and feet, especially in cooler weather</Radio>
-                            <Radio className='radio' value='b'>b)Warm hands and feet, especially in warmer weather</Radio>
-                            <Radio className='radio' value='c'>c)Generally stable body temperature</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>14. How do you handle conflicts or confrontations?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Avoid them whenever possible</Radio>
-                            <Radio className='radio' value='b'>b)Confront them directly and assertively</Radio>
-                            <Radio className='radio' value='c'>c)Seek compromise and harmony</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-
-                <FormControl as='fieldset' backgroundColor='white' borderColor='grey' borderWidth={1} padding={4} borderRadius='md' mt={4}>
-                    <FormLabel as='legend' color='black'>15. How is your general mood?</FormLabel>
-                    <RadioGroup colorScheme='blackAlpha'>
-                        <Stack direction={'column'}>                            
-                            <Radio className='radio' value='a'>a)Easily excited or enthusiastic, but also prone to anxiety</Radio>
-                            <Radio className='radio' value='b'>b)Confident and assertive, but also prone to anger</Radio>
-                            <Radio className='radio' value='c'>c)Calm and steady, but also prone to melancholy</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </FormControl>
-                
-                <Button colorScheme='teal' size='md' mt={4}>
-                    Submit
-                </Button>
-            </div>
         </div>
     );
 };
